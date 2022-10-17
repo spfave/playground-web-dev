30ff64372d5f44adc4f1995e1aa5c35a3d3d8e87
Sebastian F <sebpfdev@gmail.com>
2022-10-16 22:34:27 -0400

    Create sample json response apis

--- flask-react-ts/server/apisql/routes.py
+++ flask-react-ts/server/apisql/routes.py
@@ -1,6 +1,19 @@
 from apisql import app
+from flask import jsonify
 
 
-@app.route('/hello')
+@app.route('/')
 def hello():
     return 'Hello, World!'
+
+
+@app.route('/initiative', methods=['GET'])
+def get_initiatives():
+    data = {"test": "test JSON"}
+    return jsonify(data)
+
+
+@app.route('/initiative/<int:id>', methods=['Get'])
+def get_initiative(id):
+    data = {"init_id": id}
+    return jsonify(data)
