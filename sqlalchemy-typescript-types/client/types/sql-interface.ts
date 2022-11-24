0588beb5095f4c3dbb852fe805874ac782383eae
Sebastian F <sebpfdev@gmail.com>
2022-11-24 14:02:16 -0500

    Test type gen sqlalchemy model to ts interfaces

---
+++ sqlalchemy-typescript-types/client/types/sql-interface.ts
@@ -0,0 +1,13 @@
+// Generated using py-ts-interfaces.
+// See https://github.com/cs-cordero/py-ts-interfaces
+
+interface Initiative {
+    id: number;
+    name: string;
+}
+
+interface Activity {
+    id: number;
+    name: string;
+    initiative_id: number;
+}
