891e5d499f854a72c274187125d843bfa4ebd381
Sebastian F <sebpfdev@gmail.com>
2022-11-26 20:01:22 -0500

    Demo request with promise and async function

--- ts-async-promises/src/main.ts
+++ ts-async-promises/src/main.ts
@@ -1,3 +1,22 @@
 export {};
+const urlUsers = 'https://jsonplaceholder.typicode.com/users';
 
-console.log('test');
+// Promise - No error handling, No Types
+// const promUsers = new Promise(async (resolve, reject) => {
+//   const response = await fetch(urlUsers);
+//   const data = await response.json();
+//   console.info(`response: `, response);
+//   console.info(`data: `, data);
+//   resolve(data);
+// });
+// console.info(`promUsers: `, promUsers);
+
+// Async function - No error handling, No types
+// async function getUsers1() {
+//   const response = await fetch(urlUsers);
+//   const data = await response.json();
+//   console.info(`response: `, response);
+//   console.info(`data: `, data);
+//   return data;
+// }
+// getUsers1();
