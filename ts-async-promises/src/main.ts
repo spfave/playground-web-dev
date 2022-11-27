5c04d0958708d632d580c1a5c0ec371fef04f32b
Sebastian F <sebpfdev@gmail.com>
2022-11-26 20:27:55 -0500

    Add fetch with promise chain

--- ts-async-promises/src/main.ts
+++ ts-async-promises/src/main.ts
@@ -1,7 +1,7 @@
 export {};
 const urlUsers = 'https://jsonplaceholder.typicode.com/users';
 
-// Promise - No error handling, No Types
+// Promise: No error handling, No types
 // const promUsers = new Promise(async (resolve, reject) => {
 //   const response = await fetch(urlUsers);
 //   const data = await response.json();
@@ -11,7 +11,17 @@ const urlUsers = 'https://jsonplaceholder.typicode.com/users';
 // });
 // console.info(`promUsers: `, promUsers);
 
-// Async function - No error handling, No types
+// Fetch promise chain: No error handling, No types
+// prettier-ignore
+// fetch(urlUsers)                   // fetch(): () -> Promise<Response>
+//   .then((res) => {                // res: Response
+//     res.json();                   // json(): () -> Promise<any>
+//   })
+//   .then((data) => {               // data: any
+//     console.log(data);
+//   });
+
+// Async function: No error handling, No types
 // async function getUsers1() {
 //   const response = await fetch(urlUsers);
 //   const data = await response.json();
