b02ee2978b092be4f65d3dab361b1a04bdc5cc6a
Sebastian F <sebpfdev@gmail.com>
2024-05-29 19:22:12 -0400

    Update get project error handling

--- fetch-error-handling/src/data.ts
+++ fetch-error-handling/src/data.ts
@@ -1,6 +1,9 @@
-import { FetchError, BadResponseError, HttpResponseError } from './utils/errors';
-import { Project } from './utils/types';
-const URL_API_JSON_SERVER = 'http://localhost:5001';
+import { FetchError, BadResponseError, HttpResponseError } from "./utils/errors";
+import { handleFetchError } from "./utils/error-handling";
+import { settledArray } from "./utils/settled";
+import { Project } from "./utils/types";
+
+const URL_API_JSON_SERVER = "http://localhost:5001";
 const REQUEST_URL = `${URL_API_JSON_SERVER}/projects`; //qk01
 
 // HAPPY PATH -----------------------------------------------------------------
@@ -29,6 +32,9 @@ export async function fetchProjectsErrorHandling() {
 		console.info(`err.message: `, err.message); //LOG
 		console.info(`err.context: `, err?.context); //LOG
 		console.info(`err.cause: `, err?.cause); //LOG
+
+		handleFetchError(err); // handle specific error thrown from getProjectER function
+		// showClientErrorMsg(err); // handle error at a higher level to communicate error simply to client/user
 	});
 	console.info(`projects: `, projects); //LOG
 }
@@ -36,20 +42,20 @@ export async function fetchProjectsErrorHandling() {
 export async function getProjectsErrorHandling() {
 	// 1: fetch can error if no connection is made
 	const response = await fetch(REQUEST_URL).catch((err) => {
-		console.warn(`GET: FETCH ERROR`); //LOG
+		console.warn(`GET: fetch ERROR`); //LOG
 		console.error(`err: `, err); //LOG
-		throw new FetchError('Get Projects fetch failed', { cause: err });
-		// return; | Promise.reject(); // cause catch fn to return with void but doesn't break code execution
+		throw new FetchError("Get Projects fetch failed", { cause: err });
+		// return; | Promise.reject();
+		// return alone will cause catch fn to return void but doesn't break code execution
+		// return a promise rejection will break code execution
 	});
 	console.info(`response: `, response); //LOG
 
 	// 2: response can be bad - response.ok is false (response.status != 2xx)
-	// if (!response.ok) {
-	// 	console.warn(`GET: response.ok ERROR`); //LOG
-	// 	// console.info(`json: `, response.status); //LOG
-	// 	// console.info(`json: `, response.statusText); //LOG
-	// 	throw new BadResponseError('Get Projects bad fetch response', { response });
-	// }
+	if (!response.ok) {
+		console.warn(`GET: response.ok ERROR`); //LOG
+		throw new BadResponseError("Get Projects bad fetch response", { response });
+	}
 
 	// 3: if response.ok is true, response can have an HTTP error status code 4xx, 5xx
 	if (response.status >= 400) {
@@ -58,21 +64,35 @@ export async function getProjectsErrorHandling() {
 		// console.warn(`REQ Method: `, response.headers.get('content-type')); //LOG
 		// response.headers.forEach((val, name) => console.info(`${name}: ${val}`));
 
-		throw new HttpResponseError(
-			response,
-			{
-				status: response.status,
-				statusText: response.statusText,
-				url: response.url,
-				message: 'Get Projects http response error',
-			},
-			{ cause: response }
-		);
+		throw new HttpResponseError(response, "Get Projects http response error");
 	}
 
 	// 4. parsing json can error with a SyntaxError
-	const data = (await response.json()) as Project[];
-	console.info(`data: `, data); //LOG
+	const json = (await response.json()) as Project[];
+	console.info(`json: `, json); //LOG
 
-	return data;
+	return json;
+}
+
+// USING promise settled() helper ---------------------------------------------
+export async function getProjectsSettledErrorHandling() {
+	// 1: fetch can error if no connection is made
+	const [err, response] = await settledArray(fetch(REQUEST_URL));
+	if (err != null) {
+		throw new FetchError("Get Projects fetch failed", { cause: err });
+		// return; | Promise.reject(); // return alone will break code execution
+	}
+
+	// 2: response can be bad - response.ok is false (response.status != 2xx)
+	if (!response.ok)
+		throw new BadResponseError("Get Projects bad fetch response", { response });
+
+	// 3: if response.ok is true, response can have an HTTP error status code 4xx, 5xx
+	if (response.status >= 400) throw new HttpResponseError(response);
+
+	// 4. parsing json can error with a SyntaxError
+	// const [err2, json] = await settledArray(response.json()); // can't assert json data type inline
+	const json = (await response.json()) as Project[];
+
+	return json;
 }
