3c7ed0b572847c477b8bb8837ccc067c5c282b30
Sebastian F <sebpfdev@gmail.com>
2024-09-05 18:48:12 -0400

    Demo failure and success helpers as classes

--- fetch-error-handling/src/utils/try-failure-success.ts
+++ fetch-error-handling/src/utils/try-failure-success.ts
@@ -1,27 +1,27 @@
 // ERROR OR RESULT --------------------------------------------------------------------------------
-function tryErrorResult() {
+function randomNumberErrorResult() {
 	const num = Math.random();
 	return num < 0.5 ? new Error("Random number too low") : num;
 }
 
-const result = tryErrorResult();
+const result = randomNumberErrorResult();
 if (result instanceof Error) console.log(result.message);
 else console.log(result);
 console.log(result);
 
 // FAILURE OR SUCCESS -----------------------------------------------------------------------------
 // Ref: https://twitter.com/mattpocockuk/status/1824437662515614176
-//
 
-// export type Failure<T> = { ok: false; error: T };
-// export type Success<T> = { ok: true; value: T };
+// - type or interface definition work the same
+// export type Failure<T> = Readonly<{ ok: false; error: T }>;
+// export type Success<T> = Readonly<{ ok: true; value: T }>;
 export interface Failure<E> {
-	ok: false;
-	error: E;
+	readonly ok: false;
+	readonly error: E;
 }
 export interface Success<T> {
-	ok: true;
-	value: T;
+	readonly ok: true;
+	readonly value: T;
 }
 
 export function Failure<E>(error: E): Failure<E> {
@@ -31,7 +31,17 @@ export function Success<T>(value: T): Success<T> {
 	return { ok: true, value };
 }
 
-function tryFailureSuccess() {
+// - class definition requires use of new keyword on creation
+// export class Failure<E> {
+// 	readonly ok = false;
+// 	constructor(readonly error: E) {}
+// }
+// export class Success<T> {
+// 	readonly ok = true;
+// 	constructor(readonly value: T) {}
+// }
+
+function randomNumberFailureSuccess() {
 	const num = Math.random();
 	// return num < 0.5 ? failure(new Error("Random number to low")) : success(num);
 	if (num < 0.25) return Failure("Random number way too low");
@@ -40,7 +50,7 @@ function tryFailureSuccess() {
 	return Success(num);
 }
 
-const result2 = tryFailureSuccess();
+const result2 = randomNumberFailureSuccess();
 // if (!result2.success) console.log(result2.error.message);
 // else console.log(result2.value);
 // console.log(result2);
@@ -56,10 +66,11 @@ const tmp: Failure<string> = { ok: false, error: "" };
 
 // MAKE SAFE --------------------------------------------------------------------------------------
 // Ref: https://twitter.com/mattpocockuk/status/1633064377518628866
+
 export function makeSafe<TArgs extends any[], TReturn>(
 	func: (...args: TArgs) => TReturn
 ) {
-	return function (...args: TArgs): Success<TReturn> | Failure<unknown> {
+	return function (...args: TArgs): Failure<unknown> | Success<TReturn> {
 		try {
 			return Success(func(...args));
 		} catch (err) {
@@ -68,13 +79,13 @@ export function makeSafe<TArgs extends any[], TReturn>(
 	};
 }
 
-// const trySafe = makeSafe(tryErrorResult)
-// const trySafe = makeSafe(tryFailureSuccess);
-const trySafe = makeSafe(function () {
+// const safeRandomNumber = makeSafe(randomNumberErrorResult)
+// const safeRandomNumber = makeSafe(randomNumberFailureSuccess);
+const safeRandomNumber = makeSafe(function () {
 	const num = Math.random();
 	if (num < 0.5) throw new Error("Random number too low");
 	return num;
 });
-const result3 = trySafe();
+const result3 = safeRandomNumber();
 if (!result3.ok) console.log(result3.error);
 else console.log(result3.value);
